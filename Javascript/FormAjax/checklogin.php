ac51179a77187f73dcd1804512006f4a21722972
hungnd <hungnd114@gmail.com>
2017-10-30 10:24:38 +0700

    update check validate in php

--- Javascript/FormAjax/checklogin.php
+++ Javascript/FormAjax/checklogin.php
@@ -1,28 +1,56 @@
 <?php 
 	$db_host = "localhost";
 	$db_username = "root";
-	//$db_password = '';
+	$db_password = '';
 	$db_name = "formajax";
 
 	// connect to MySQL Server
-	$connect = mysqli_connect($db_host, $db_username, "");
+	$connect = mysqli_connect($db_host, $db_username, $db_password);
 
 	//connect to database
 	mysqli_select_db($connect, $db_name) or die(mysqli_errno($connect));
 
-	//get data
+	//get data	
 	$username = $_GET["username"];
 	$password = $_GET["password"];
-	$query = "select * from user where username = '$username'";
-	$member = mysqli_query($connect, $query) or die(mysqli_errno($connect));
-	$row = mysqli_fetch_assoc($member);
-	if ($row != null) {
-		if ($row['password'] == $password) {
-			echo '<p class="success">Login thành công !.</p>';
-		}else{
-			echo '<p class="success">Password không đúng !</p>';
+
+	if (checkInputUsername($username) == true && checkInputPassword($password) == true) {
+		$query = "select * from user where username = '$username'";
+		$member = mysqli_query($connect, $query) or die(mysqli_errno($connect));
+		$row = mysqli_fetch_assoc($member);
+		if ($row != null) {
+			if ($row['password'] == $password) {
+				echo '<p class="success">Login thành công !.</p>';
+			}else{
+				echo '<p class="success">Password không đúng !</p>';
+			}
+		}else {
+			echo '<p class="success">Username hoặc password không đúng !</p>';
 		}
-	}else {
-		echo '<p class="success">Username hoặc password không đúng !</p>';
+	} else {
+		echo '<p class="success">input fail...</p>';
 	}
+
+	//check username received
+	function checkInputUsername($username) {
+		if (strlen($username) < 8 || strlen($username) < 8) {
+			echo '<p class="success">Username in 8-50 character.</p>';
+			return false;
+		}
+		if (preg_match('/[\'^£$%&*()}{@#~?><>,|=_+¬-]/', $username)){
+		   echo '<p class="success">Not enter special character.</p>';
+		   return false;
+		}
+		return true;
+	}
+	
+	//check password received
+	function checkInputPassword($password) {
+		if (strlen($password) < 8 || strlen($password) < 8) {
+			echo '<p class="success">Password in 8-50 character.</p>';
+			return false;
+		}
+		return true;
+	}
+	
  ?>
